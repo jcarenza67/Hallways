0c198c884691dfb5e551ac716c9faf47420b412b
Joseph Wilfong <josephwilfong91@gmail.com>
2023-07-21 11:59:01 -0700

    fix the flickering light to return to normal after switching back to scene.

--- Assets/LampEmissionController.cs
+++ Assets/LampEmissionController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class LampEmissionController : MonoBehaviour
 {
     public Material lampMaterial; // assign in Inspector
+    private Material initialMaterial;
+    private Material flickerMaterial;
     public Color initialEmissionColor = Color.black; // initial emission when light is off
     public float maxEmission = 1.0f; // max emission when light is on
     public float minEmission = 0.0f; // min emission when light is off
     public FlickeringLight flickeringLight; // assign in Inspector
 
     private Color baseColor;
+    private bool isFlickering = false;
 
     private void Start()
     {
+        initialMaterial = GetComponent<Renderer>().material;
         // get the base color of the material's emission
         baseColor = lampMaterial.GetColor("_EmissionColor");
         lampMaterial.SetColor("_EmissionColor", initialEmissionColor);
@@ -21,7 +25,27 @@ public class LampEmissionController : MonoBehaviour
 
     private void Update()
     {
-        float emission = flickeringLight.lightSource.enabled ? maxEmission : minEmission;
-        lampMaterial.SetColor("_EmissionColor", baseColor * Mathf.LinearToGammaSpace(emission));
+        if (flickeringLight.lightSource.enabled)
+        {
+            if (!isFlickering)
+            {
+                isFlickering = true;
+                flickerMaterial = new Material(lampMaterial);
+                GetComponent<Renderer>().material = flickerMaterial;
+            }
+            float emission = maxEmission;
+            flickerMaterial.SetColor("_EmissionColor", baseColor * Mathf.LinearToGammaSpace(emission));
+        }
+        else
+        {
+            if (isFlickering)
+            {
+                isFlickering = false;
+                GetComponent<Renderer>().material = initialMaterial;
+            }
+            float emission = minEmission;
+            initialMaterial.SetColor("_EmissionColor", baseColor * Mathf.LinearToGammaSpace(emission));
+        }
     }
 }
+
