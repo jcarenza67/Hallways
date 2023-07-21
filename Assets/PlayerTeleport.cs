3d7c9b4330da5185473c79894406dce59a0436ca
Joseph Wilfong <josephwilfong91@gmail.com>
2023-07-21 14:26:39 -0700

    teleportation is working

--- Assets/PlayerTeleport.cs
+++ Assets/PlayerTeleport.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class PlayerTeleport : MonoBehaviour
 {
     public Transform teleportTarget;
+    public CharacterController controller;
 
     private void OnTriggerEnter(Collider other)
     {
+      Debug.Log("Trigger entered");
         if (other.gameObject.CompareTag("FinalDoor"))
         {
+          Debug.Log("Teleporting");
+            controller.enabled = false;
             transform.position = teleportTarget.position;
+            transform.rotation = teleportTarget.rotation;
+            controller.enabled = true;
         }
     }
 }
