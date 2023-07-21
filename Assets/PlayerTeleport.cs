272bc23387db9daff8f8e2d591d1f3d3bd25822a
Joseph Wilfong <josephwilfong91@gmail.com>
2023-07-21 15:32:24 -0700

    fade to black, teleport player, and they will show up at the beginning again

--- Assets/PlayerTeleport.cs
+++ Assets/PlayerTeleport.cs
@@ -1,21 +1,46 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerTeleport : MonoBehaviour
 {
     public Transform teleportTarget;
+    public Image fadeImage;
     public CharacterController controller;
 
     private void OnTriggerEnter(Collider other)
     {
-      Debug.Log("Trigger entered");
+        Debug.Log("Trigger entered");
         if (other.gameObject.CompareTag("FinalDoor"))
         {
-          Debug.Log("Teleporting");
-            controller.enabled = false;
-            transform.position = teleportTarget.position;
-            transform.rotation = teleportTarget.rotation;
-            controller.enabled = true;
+            Debug.Log("Teleporting");
+            StartCoroutine(TeleportAndFade());
+        }
+    }
+
+    IEnumerator TeleportAndFade()
+    {
+        for (float t = 0.0f; t <= 1.0f; t += Time.deltaTime)
+        {
+            Color c = fadeImage.color;
+            c.a = t;
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        controller.enabled = false;
+        transform.position = teleportTarget.position;
+        transform.rotation = teleportTarget.rotation;
+        controller.enabled = true;
+
+        for (float t = 1.0f; t >= 0.0f; t -= Time.deltaTime)
+        {
+            Color c = fadeImage.color;
+            c.a = t;
+            fadeImage.color = c;
+            yield return null;
         }
     }
 }
+
 
