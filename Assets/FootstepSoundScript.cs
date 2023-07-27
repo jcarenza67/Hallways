e23615a651ec86c4e64eee77ad05cbf1fbb395d8
Joseph Wilfong <josephwilfong91@gmail.com>
2023-07-26 17:10:23 -0700

    speed up footstep audio when player runs. toned down the bathroom voice and added reverb

--- Assets/FootstepSoundScript.cs
+++ Assets/FootstepSoundScript.cs
@@ -6,12 +6,15 @@ public class FootstepSoundScript : MonoBehaviour
     public AudioClip runningSound;
     public float walkingSpeed = 2.0f;
     public float runningSpeed = 5.0f;
-    public float footstepCooldown = 0.5f; // Adjust this value to control the footstep sound delay
+
+    public float walkingFootstepCooldown = 0.5f;
+    public float runningFootstepCooldown = 0.3f;
 
     private CharacterController characterController;
     private AudioSource walkingAudioSource;
     private AudioSource runningAudioSource;
     private bool canPlayFootstep = true;
+    private float currentFootstepCooldown;
 
     void Start()
     {
@@ -22,13 +25,13 @@ public class FootstepSoundScript : MonoBehaviour
 
     void Update()
     {
-        // Check if the player is moving and play the corresponding footstep sound
         if (characterController.isGrounded && characterController.velocity.magnitude > 0.1f)
         {
             if (characterController.velocity.magnitude <= walkingSpeed)
             {
                 if (canPlayFootstep)
                 {
+                    currentFootstepCooldown = walkingFootstepCooldown;
                     PlayFootstepSound(walkingAudioSource, walkingSound);
                 }
             }
@@ -36,6 +39,7 @@ public class FootstepSoundScript : MonoBehaviour
             {
                 if (canPlayFootstep)
                 {
+                    currentFootstepCooldown = runningFootstepCooldown;
                     PlayFootstepSound(runningAudioSource, runningSound);
                 }
             }
@@ -46,7 +50,7 @@ public class FootstepSoundScript : MonoBehaviour
     {
         audioSource.PlayOneShot(soundClip);
         canPlayFootstep = false;
-        Invoke(nameof(ResetFootstepCooldown), footstepCooldown);
+        Invoke(nameof(ResetFootstepCooldown), currentFootstepCooldown);
     }
 
     private void ResetFootstepCooldown()
